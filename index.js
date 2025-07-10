ec2e7f64b3ce99987de4e0d3c73feb5a3154f7b9
Aikokul <ajkokulnurlankyzy8@gmail.com>
2025-07-10 09:46:47 +0600

    connect cards

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 console.log();
+
+
+// js aikokul 
+const bCards = document.querySelector(".b-cards")
+
+const cards = [
+    {
+        icon: "./img/home.svg",
+        name: "Торговый эквайринг",
+        pText: "Когда клиент оплачивает счет через тап или пин код.",
+        p2Text: "2.2% за транзакцию или 0.4% через СБП",
+        bgColor: "#D9E1FF"
+    },
+    {
+        icon: "./img/mobail.svg",
+        name: "Мобильное <br/> приложение",
+        pText: "Когда ваш клиент делает покупку через мобильное приложение в заведении или на доставку.",
+        p2Text: "2.99% за транзакцию или 0.4% через СБП",
+        bgColor: " rgba(244, 231, 246, 1)"
+    },
+    {
+        icon: "./img/kart.svg",
+        name: "Онлайн",
+        pText: "Когда ваш клиент делает покупку через сайт на самовывоз или доставку.",
+        p2Text: "2.99% за транзакцию или 0.4% через СБП",
+        bgColor: "rgba(255, 233, 218, 1)"
+    }
+]
+
+for (const c of cards) {
+    bCards.innerHTML += `
+            <div class="b-card" style="background-color: ${c.bgColor} ;">
+                <div class="bg">
+                    <img class="card-icon" src="${c.icon}" alt="">
+                </div>
+                <h3>${c.name}</h3>
+                <p class="p1">${c.pText}</p>
+                <p class="p2">${c.p2Text}</p>
+            </div>
+    `
+}
+// js aikokul  end 
